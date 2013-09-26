821cc8f617f8c353193a558835f829a6d8eabc56
Erlend Røsjø <erlend.rosjo@knowit.no>
2013-09-26 20:05:51 +0200

    Removed unused code, refactoring

---
+++ Raytracer.Calculus/Objects/SceneObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Raytracer.Calculus
+{
+    public class SceneObject
+    {
+        private Color _color;
+        public virtual Color Color
+        {
+            get
+            {
+                return _color;
+            }
+            protected set
+            {
+                _color = value;
+            }
+        }
+
+        public virtual double FindIntersection(Ray ray)
+        {
+            Console.WriteLine("Wrong find intersection");
+            return 0d;
+        }
+    }
+}
