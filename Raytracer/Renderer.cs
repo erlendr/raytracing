b83e2ede0d9e9be00705328905dfd7e2e0db7cd5
Erlend Røsjø <erlend.rosjo@knowit.no>
2013-09-28 18:49:22 +0200

    Renderer now computes Lambert lighting for spheres

--- Raytracer/Renderer.cs
+++ Raytracer/Renderer.cs
@@ -17,6 +17,8 @@ namespace Raytracer
         readonly Vect _x = new Vect(1, 0, 0);
         readonly Vect _y = new Vect(0, 1, 0);
         readonly Vect _z = new Vect(0, 0, 1);
+        private const double AmbientCoefficient = 0.1d;
+        private const double DiffuseCoefficient = 1d;
         private const double Accuracy = 0.00000001;
 
         public Renderer()
@@ -25,6 +27,7 @@ namespace Raytracer
 
             var whiteLight = new Color(1.0d, 1.0d, 1.0d, 0d);
             var prettyGreen = new Color(0.5d, 1.0d, 0.5d, 0.3d);
+            var red = new Color(1d, 0.0d, 0d, 0.3d);
             var gray = new Color(0.5d, 0.5d, 0.5d, 0d);
             var black = new Color(0d, 0d, 0d, 0d);
             var maroon = new Color(0.5d, 0.25d, 0.25d, 0);
@@ -32,11 +35,13 @@ namespace Raytracer
             var lightPosition = new Vect(-7, 10, -10);
             var light = new Light(lightPosition, whiteLight);
 
-            var sceneSphere = new Sphere(_o, 1.0d, prettyGreen);
+            var sceneSphere = new Sphere(_o.Add(new Vect(1d, -0.5d, -1.5d)), 0.5d, red);
+            var sceneSphere2 = new Sphere(_o, 1.0d, prettyGreen);
             var scenePlane = new Plane(_y, -1, maroon);
             var sceneObjects = new List<SceneObject>
                                    {
                                        sceneSphere,
+                                       sceneSphere2,
                                        scenePlane
                                    };
 
@@ -70,7 +75,7 @@ namespace Raytracer
                         var camRayOrigin = camera.CameraPosition;
                         var camRayDirection =
                             camera.CameraDirection.Add(
-                                camera.CameraRight.Mult(xamnt - 0.5d).Add(camera.CameraDown.Mult(yamnt - 0.5))).
+                                camera.CameraRight.Mult(xamnt - 0.5d).Add(camera.CameraDown.Mult(yamnt - 0.5d))).
                                 Normalize();
 
                         var camRay = new Ray(camRayOrigin, camRayDirection);
@@ -88,13 +93,36 @@ namespace Raytracer
                         var b = 0;
                         if (indexOfWinningObject > -1)
                         {
+                            double shade = 1d;
+
                             if (intersections[indexOfWinningObject] > Accuracy)
                             {
                                 var winningObject = sceneObjects[indexOfWinningObject];
-                       
-                                r = (int) Math.Round(winningObject.Color.Red*255);
-                                g = (int) Math.Round(winningObject.Color.Blue*255);
-                                b = (int) Math.Round(winningObject.Color.Green*255);
+                                if (winningObject.GetType().Name.Contains("Sphere"))
+                                {
+                                    //compute light ray
+                                    var intersectionPoint =
+                                        camRayOrigin.Add(camRayDirection.Mult(intersections[indexOfWinningObject]));
+                                    var diffBtwIntersectionAndLightPos = new Vect(intersectionPoint.X - lightPosition.X, intersectionPoint.Y - lightPosition.Y, intersectionPoint.Z - lightPosition.Z);
+                                    var lightDirectionFromIntersectionPoint = diffBtwIntersectionAndLightPos.Negative().Normalize();
+                                    var winningObjectNormal = winningObject.GetNormalAt(intersectionPoint);
+                                    var angleBetweenNormalAndLightDirection =
+                                        winningObjectNormal.DotProduct(lightDirectionFromIntersectionPoint);
+                                    Console.WriteLine("Angle:" + angleBetweenNormalAndLightDirection);
+
+                                    if(angleBetweenNormalAndLightDirection < 0)
+                                    {
+                                        angleBetweenNormalAndLightDirection = 0;
+                                    }
+
+                                    shade = (AmbientCoefficient +
+                                                          DiffuseCoefficient*angleBetweenNormalAndLightDirection);
+                                }
+                                
+                                
+                                r = (int) Math.Min(Math.Round(winningObject.Color.Red*shade*255), 255);
+                                g = (int) Math.Min(Math.Round(winningObject.Color.Blue*shade*255), 255);
+                                b = (int) Math.Min(Math.Round(winningObject.Color.Green*shade*255), 255);
                             }
                         }
                         
